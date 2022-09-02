78771d07c8c38ff1003f186820b263490edf477e
LPandorf <leticiapandorf@gmail.com>
2022-09-01 21:15:44 -0300

    Revert "Tela 1 centralizada e selecionando quizz para a tela 2"
    This reverts commit a232ad0fee05bb6805e21d3c32a41917875cca0e.

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const URL_API = 'https://mock-api.driven.com.br/api/v4/buzzquizz/';
 const quizzes = [];
-
+function getQuizzes() {
+    const promessa = axios.get(`${URL_API}/quizzes`);
+    promessa.then(showQuizzes);
+}
 
 function showQuizzes(promessa) {
-    const elementoQuizz = document.querySelector('.quizzes-list');
-    elementoQuizz.innerHTML = '';
+    console.log(promessa.data);
+    const elementoQuizz = document.querySelector('.all-quizzes');
 
+    elementoQuizz.innerHTML = '';
     for (let i = 0; i < promessa.data.length; i++)
         quizzes.push(promessa.data[i]);
-
+    console.log(quizzes);
     for (let i = 0; i < quizzes.length; i++) {
-        elementoQuizz.innerHTML += `<div class="quizz" 
-        id="${quizzes[i].id}" onclick="selectQuizz(this)">
+        elementoQuizz.innerHTML += `<div class="quizz" onclick="selectQuizz()">
             <p>
-                ${quizzes[i].title} 
+                ${quizzes[i].title}
             </p>
         </div>`
     }
-
     const elementos = document.querySelectorAll('.quizz');
-
     for (let i = 0; i < elementos.length; i++) {
         elementos[i].style.backgroundImage = `linear-gradient(to bottom, 
             rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.8))
@@ -28,68 +29,9 @@ function showQuizzes(promessa) {
     }
 }
 
-function getQuizzes() {
-    const promessa = axios.get(`${URL_API}/quizzes`);
-    promessa.then(showQuizzes);
-}
-
-function iniciaQuizz(indiceQuizz) {
-    const elementoEscondido = document.querySelector('.conteudo-principal');
-    elementoEscondido.classList.add('hidden');
-    const quizzSelecionado = document.querySelector('.quizz-selected');
-    quizzSelecionado.classList.remove('hidden');
-    const logo = document.querySelector('.quizz-logo');
-    logo.scrollIntoView({
-        block: "end",
-        inline: "start",
-        behavior: "smooth"
-    });
-    logo.firstElementChild.innerHTML = `${quizzes[indiceQuizz].title}`;
-    logo.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.57), 
-rgba(0,0,0,0.57)), url(${quizzes[indiceQuizz].image})`;
-    logo.style.backgroundSize = "25% 100%";
-    const perguntaQuizz = document.querySelector('.quizz-question');
-    perguntaQuizz.innerHTML = '';
-
-    for (let i = 0; i < quizzes[indiceQuizz].questions.length; i++) {
-        perguntaQuizz.innerHTML += `<div class="quizz-question-title">
-            <h1>
-                ${quizzes[indiceQuizz].questions[i].title}
-            </h1>
-        </div>
-        <div class="quizz-selected-images">
-            <div>
-                <div class="quizz-selected-option">
-                </div>
-                <p>
-                    ${quizzes[indiceQuizz].questions[i].answers[0].text}
-                </p>
-                <div class="quizz-selected-option">
-                </div>
-                <p>
-                    ${quizzes[indiceQuizz].questions[i].answers[1].text}
-                </p>
-            </div>
-        </div>`;
-    }
-    const imageQuizz = document.querySelector('.quizz-question-title');
-    imageQuizz.style.backgroundColor = quizzes[indiceQuizz].questions[0].color;
-    const imagesQuestionsQuizz = document.querySelectorAll('.quizz-selected-option')
-    for (let i = 0; i < imagesQuestionsQuizz.length; i++) {
-        imagesQuestionsQuizz[i].style.backgroundImage = `${quizzes[indiceQuizz].questions[i].image}`;
-    }
-
-}
-
-function selectQuizz(seletor) {
-
-    const quizzId = Number(seletor.id);
-    for (let i = 0; i < quizzes.length; i++)
-        if (quizzId === quizzes[i].id) {
-            const indiceQuizzSelecionado = i;
-            iniciaQuizz(indiceQuizzSelecionado);
-        }
-
+function selectQuizz() {
+    const elemento = document.querySelector('.conteudo-principal');
+    elemento.classList.add('hidden');
 }
 
 getQuizzes();
