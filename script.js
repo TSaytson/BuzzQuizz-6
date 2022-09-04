b551e23c630d81163bbda525a3fa02111471b196
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2022-09-04 06:15:41 -0300

    exibindo respostas na tela 2 organizadas aleatoriamente

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const URL_API = 'https://mock-api.driven.com.br/api/v4/buzzquizz/';
 const quizzes = [];
-
+let indiceQuizzSelecionado = null;
 
 function showQuizzes(promessa) {
     const elementoQuizz = document.querySelector('.quizzes-list');
@@ -33,7 +33,16 @@ function getQuizzes() {
     promessa.then(showQuizzes);
 }
 
-function buildQuestionsStructure(indiceQuizz) {
+function verifyAnswer(seletor, indiceQuizzSelecionado) {
+
+    for (let i = 0; i < quizzes[indiceQuizzSelecionado].questions.length; i++) {
+        if (quizzes[indiceQuizzSelecionado].questions[i].title == seletor.parentNode.previousElementSibling.innerHTML) {
+
+        }
+    }
+}
+
+function buildQuestionsStructure(indiceQuizzSelecionado) {
     const quizzSelected = document.querySelector(".quizz-selected");
     quizzSelected.innerHTML = "";
     quizzSelected.innerHTML += `<div class="quizz-logo">
@@ -45,17 +54,17 @@ function buildQuestionsStructure(indiceQuizz) {
         inline: "start",
         behavior: "smooth"
     });
-    logo.firstElementChild.innerHTML = `${quizzes[indiceQuizz].title}`;
+    logo.firstElementChild.innerHTML = `${quizzes[indiceQuizzSelecionado].title}`;
     logo.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.57), 
-rgba(0,0,0,0.57)), url(${quizzes[indiceQuizz].image})`;
+rgba(0,0,0,0.57)), url(${quizzes[indiceQuizzSelecionado].image})`;
     logo.style.backgroundSize = "360px 230px";
 
-    for (let i = 0; i < quizzes[indiceQuizz].questions.length; i++) {
+    for (let i = 0; i < quizzes[indiceQuizzSelecionado].questions.length; i++) {
         quizzSelected.innerHTML += `
         <div class="quizz-question">
             <div class="quizz-question-title">
                 <h1>
-                    ${quizzes[indiceQuizz].questions[i].title}
+                    ${quizzes[indiceQuizzSelecionado].questions[i].title}
                 </h1>
             </div>
             <div class="quizz-selected-images">
@@ -67,43 +76,55 @@ rgba(0,0,0,0.57)), url(${quizzes[indiceQuizz].image})`;
 
     console.log(`quizzQuestions.length: `, quizzQuestions.length);
 
-    for (let i = 0; i < quizzes[indiceQuizz].questions.length; i++) {
-        for (j = 0; j < quizzes[indiceQuizz].questions[i].answers.length; j++) {
+    for (let i = 0; i < quizzes[indiceQuizzSelecionado].questions.length; i++) {
+        for (j = 0; j < quizzes[indiceQuizzSelecionado].questions[i].answers.length; j++) {
             quizzQuestions[i].firstElementChild.nextElementSibling.innerHTML += `
-            <div>
-                <div onclick="verifyAnswer(this)" class="quizz-selected-option">
+            <div onclick="verifyAnswer(this)">
+                <div class="quizz-selected-option">
                 </div>
                 <p>
                 </p>
             </div>`;
-            quizzQuestions[i].firstElementChild.style.backgroundColor = `${quizzes[indiceQuizz].questions[i].color}`;
+            quizzQuestions[i].firstElementChild.style.backgroundColor = `${quizzes[indiceQuizzSelecionado].questions[i].color}`;
         }
     }
 
     const questionsContent = document.querySelectorAll('.quizz-selected-option');
-    k = 0;
-    for (let i = 0; i < quizzes[indiceQuizz].questions.length; i++) {
-        for (let j = 0; j < quizzes[indiceQuizz].questions[i].answers.length; j++) {
-            questionsContent[k].style.backgroundImage = `
-        url(${quizzes[indiceQuizz].questions[i].answers[j].image})`;
+    let k = 0, randomNumber = 0;
 
-            questionsContent[k].style.backgroundSize = "320px 180px";
+    for (let i = 0; i < quizzes[indiceQuizzSelecionado].questions.length; i++) {
+        let randomNumbers = [];
+        for (let j = 0; j < quizzes[indiceQuizzSelecionado].questions[i].answers.length; j++) {
+            randomNumbers.push(j);
+        }
+        for (let j = quizzes[indiceQuizzSelecionado].questions[i].answers.length; j;) {
+            randomNumber = Math.random() * j-- | 0;
+            tmp = randomNumbers[randomNumber];
+            randomNumbers[randomNumber] = randomNumbers[j];
+            randomNumbers[j] = tmp;
+        }
+
+        for (let j = 0; j < quizzes[indiceQuizzSelecionado].questions[i].answers.length; j++) {
+            questionsContent[k].style.backgroundImage = `
+        url(${quizzes[indiceQuizzSelecionado].questions[i].answers[randomNumbers[j]].image})`;
+
+            questionsContent[k].style.backgroundSize = "340px 180px";
             questionsContent[k].style.cursor = "pointer";
 
             questionsContent[k].nextElementSibling.innerHTML = `
-        ${quizzes[indiceQuizz].questions[i].answers[j].text}`;
+        ${quizzes[indiceQuizzSelecionado].questions[i].answers[randomNumbers[j]].text}`;
             k++;
         }
     }
 }
 
-function startQuizz(indiceQuizz) {
+function startQuizz(indiceQuizzSelecionado) {
     const elementoEscondido = document.querySelector(".conteudo-principal");
     elementoEscondido.classList.add("hidden");
     const quizzSelecionado = document.querySelector(".quizz-selected");
     quizzSelecionado.classList.remove("hidden");
 
-    buildQuestionsStructure(indiceQuizz);
+    buildQuestionsStructure(indiceQuizzSelecionado);
 
 }
 
@@ -112,7 +133,7 @@ function selectQuizz(seletor) {
     const quizzId = Number(seletor.id);
     for (let i = 0; i < quizzes.length; i++)
         if (quizzId === quizzes[i].id) {
-            const indiceQuizzSelecionado = i;
+            indiceQuizzSelecionado = i;
             startQuizz(indiceQuizzSelecionado);
         }
 
